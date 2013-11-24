7e7378110d25b5dc76f825d9fe4120de41dc7d1c
Vitor Antero <vitor.melo@gmail.com>
2013-11-24 22:19:14 +0000

    Adds south migrations to database

---
+++ core/migrations/0001_initial.py
@@ -0,0 +1,163 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'CustomUser'
+        db.create_table(u'core_customuser', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('password', self.gf('django.db.models.fields.CharField')(max_length=128)),
+            ('last_login', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
+            ('is_superuser', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('username', self.gf('django.db.models.fields.CharField')(unique=True, max_length=25)),
+            ('email', self.gf('django.db.models.fields.EmailField')(unique=True, max_length=255)),
+            ('name', self.gf('django.db.models.fields.CharField')(default='', max_length=100, null=True, blank=True)),
+            ('signup_date', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime(2013, 11, 24, 0, 0))),
+            ('nickname', self.gf('django.db.models.fields.CharField')(default='', max_length=100, null=True, blank=True)),
+            ('is_active', self.gf('django.db.models.fields.BooleanField')(default=True)),
+            ('is_admin', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('is_staff', self.gf('django.db.models.fields.BooleanField')(default=False)),
+        ))
+        db.send_create_signal(u'core', ['CustomUser'])
+
+        # Adding M2M table for field groups on 'CustomUser'
+        m2m_table_name = db.shorten_name(u'core_customuser_groups')
+        db.create_table(m2m_table_name, (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('customuser', models.ForeignKey(orm[u'core.customuser'], null=False)),
+            ('group', models.ForeignKey(orm[u'auth.group'], null=False))
+        ))
+        db.create_unique(m2m_table_name, ['customuser_id', 'group_id'])
+
+        # Adding M2M table for field user_permissions on 'CustomUser'
+        m2m_table_name = db.shorten_name(u'core_customuser_user_permissions')
+        db.create_table(m2m_table_name, (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('customuser', models.ForeignKey(orm[u'core.customuser'], null=False)),
+            ('permission', models.ForeignKey(orm[u'auth.permission'], null=False))
+        ))
+        db.create_unique(m2m_table_name, ['customuser_id', 'permission_id'])
+
+        # Adding model 'Product'
+        db.create_table(u'core_product', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=30)),
+            ('description', self.gf('django.db.models.fields.CharField')(max_length=500)),
+            ('category', self.gf('django.db.models.fields.CharField')(default='NA', max_length=5)),
+        ))
+        db.send_create_signal(u'core', ['Product'])
+
+        # Adding model 'Bid'
+        db.create_table(u'core_bid', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('bidder', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['core.CustomUser'])),
+            ('date', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime(2013, 11, 24, 0, 0))),
+            ('value', self.gf('django.db.models.fields.FloatField')()),
+        ))
+        db.send_create_signal(u'core', ['Bid'])
+
+        # Adding model 'Auction'
+        db.create_table(u'core_auction', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('auctioneer', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['core.CustomUser'])),
+            ('date_begin', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime(2013, 11, 24, 0, 0))),
+            ('date_end', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime(2013, 11, 24, 0, 0))),
+            ('product', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['core.Product'], unique=True)),
+            ('auction_type', self.gf('django.db.models.fields.CharField')(default='BRIT', max_length=5)),
+            ('start_price', self.gf('django.db.models.fields.FloatField')(default=0)),
+            ('min_price', self.gf('django.db.models.fields.FloatField')(default=0)),
+        ))
+        db.send_create_signal(u'core', ['Auction'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'CustomUser'
+        db.delete_table(u'core_customuser')
+
+        # Removing M2M table for field groups on 'CustomUser'
+        db.delete_table(db.shorten_name(u'core_customuser_groups'))
+
+        # Removing M2M table for field user_permissions on 'CustomUser'
+        db.delete_table(db.shorten_name(u'core_customuser_user_permissions'))
+
+        # Deleting model 'Product'
+        db.delete_table(u'core_product')
+
+        # Deleting model 'Bid'
+        db.delete_table(u'core_bid')
+
+        # Deleting model 'Auction'
+        db.delete_table(u'core_auction')
+
+
+    models = {
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'core.auction': {
+            'Meta': {'object_name': 'Auction'},
+            'auction_type': ('django.db.models.fields.CharField', [], {'default': "'BRIT'", 'max_length': '5'}),
+            'auctioneer': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['core.CustomUser']"}),
+            'date_begin': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 11, 24, 0, 0)'}),
+            'date_end': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 11, 24, 0, 0)'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'min_price': ('django.db.models.fields.FloatField', [], {'default': '0'}),
+            'product': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['core.Product']", 'unique': 'True'}),
+            'start_price': ('django.db.models.fields.FloatField', [], {'default': '0'})
+        },
+        u'core.bid': {
+            'Meta': {'object_name': 'Bid'},
+            'bidder': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['core.CustomUser']"}),
+            'date': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 11, 24, 0, 0)'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'value': ('django.db.models.fields.FloatField', [], {})
+        },
+        u'core.customuser': {
+            'Meta': {'object_name': 'CustomUser'},
+            'email': ('django.db.models.fields.EmailField', [], {'unique': 'True', 'max_length': '255'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Group']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_admin': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'name': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'nickname': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'signup_date': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 11, 24, 0, 0)'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '25'})
+        },
+        u'core.product': {
+            'Meta': {'object_name': 'Product'},
+            'category': ('django.db.models.fields.CharField', [], {'default': "'NA'", 'max_length': '5'}),
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '500'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '30'})
+        }
+    }
+
+    complete_apps = ['core']
