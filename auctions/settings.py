25b807e9b1b23c57d20da276fe5f5636227cf39d
Vitor Antero <vitor.melo@gmail.com>
2014-01-07 06:56:48 +0000

    Adds default DB

--- auctions/settings.py
+++ auctions/settings.py
@@ -28,6 +28,17 @@ ADMINS = (
     ('AuctionZ admin', 'auctionz.corp@gmail.com'),
 )
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'auctions.db',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
