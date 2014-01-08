73e478748b0cb8540515f2e2dc2ceb89f6f1a9ee
Matheus <matheus_giovanni@yahoo.com.br>
2014-01-08 10:21:07 +0000

    comments - js

--- auctions/static/js/login.js
+++ auctions/static/js/login.js
@@ -1,10 +1,13 @@
+//used to avoid empty inputs
 $(document).ready( function() {
+                  //login form - header
                   $('#submit_login').click(function(e){
                                      if ($('#username_login').val() === "" || $('#password_login').val()==="") {
                                      $('#error_login').html("<b>Error:</b> All the fields are required!");
                                      e.preventDefault();
                                      }
                                      });
+                  //login form - login.html
                   $('#submit_page').click(function(e){
                                      if ($('#username_page').val() === "" || $('#password_page').val()==="") {
                                      $('#error_page').html("<b>Error:</b> All the fields are required!");
--- auctions/static/js/show-details.js
+++ auctions/static/js/show-details.js
@@ -1,10 +1,13 @@
+// used to display the hidden div of each auction item
 $(document).ready(function() {
   $("div.enterleave")
   .mouseenter(function() {
+              //when the mouse enter, change the display to block
               $(this).find(".hidden").css("display","block");
               $(this).css("border-bottom", "3px solid #022D71");
               })
   .mouseleave(function() {
+              //when it leaves, change it to the hidden mode again
               $(this).find(".hidden").css("display","none");
               $(this).css("border-bottom", "3px solid #fff");
               });
